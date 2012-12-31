e29e957436fd51a7d1de2c8486efebbb8e7375d9
mmzi100266 <mmzi100266@163.com>
2012-12-31 20:39:22 +0800

    add 事务

---
+++ src/main/java/cn/sunjiachao/s7common/model/rowmapper/BlogDtoRowMapper.java
@@ -0,0 +1,21 @@
+package cn.sunjiachao.s7common.model.rowmapper;
+
+
+import cn.sunjiachao.s7common.model.dto.BlogDto;
+import org.springframework.jdbc.core.RowMapper;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class BlogDtoRowMapper<BlogDto> implements RowMapper<T> {
+    @Override
+    public BlogDto mapRow(ResultSet resultSet, int rowNum) throws SQLException {
+        BlogDto bd = new BlogDto();
+        bd.setBlogId(resultSet.getInt("blogId"));
+        bd.setTitle(resultSet.getString("title"));
+        bd.setShortBody(resultSet.getString("shortBody"));
+        bd.setCreateTime(resultSet.getDate("createTime"));
+        bd.setLoginName(resultSet.getString("loginName"));
+        return bd;
+    }
+}
