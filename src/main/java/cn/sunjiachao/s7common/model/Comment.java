28630a533838213defa5c6485aea8abb3c3e3596
mmzi100266 <mmzi100266@163.com>
2013-01-08 21:49:40 +0800

    add comments

---
+++ src/main/java/cn/sunjiachao/s7common/model/Comment.java
@@ -0,0 +1,55 @@
+package cn.sunjiachao.s7common.model;
+
+import java.util.Date;
+
+/**
+ * 留言实体
+ */
+public class Comment {
+
+    private int cid;
+    private String title;
+    private String body;
+    private Date createTime;
+    private int belongTo;
+
+    public int getCid() {
+        return cid;
+    }
+
+    public void setCid(int cid) {
+        this.cid = cid;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getBody() {
+        return body;
+    }
+
+    public void setBody(String body) {
+        this.body = body;
+    }
+
+    public Date getCreateTime() {
+        return createTime;
+    }
+
+    public void setCreateTime(Date createTime) {
+        this.createTime = createTime;
+    }
+
+    public int getBelongTo() {
+        return belongTo;
+    }
+
+    public void setBelongTo(int belongTo) {
+        this.belongTo = belongTo;
+    }
+}
